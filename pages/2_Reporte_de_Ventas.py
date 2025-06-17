aae853d863af8ea34d5b216ef6766f80f5e0974b
LindaMunarriz <crea.lindam@gmail.com>
2025-06-17 16:20:52 -0500

    Create 2_Reporte_de_Ventas.py
    Construyend reporte de ventas

---
+++ pages/2_Reporte_de_Ventas.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("🧾 Reporte de Ventas")
+    st.write("Aquí irán los formularios para registrar tus ventas.")
+    st.write("✨ Estamos construyendo esta parte mágica...")
+
+if __name__ == "__main__":
+    main()
