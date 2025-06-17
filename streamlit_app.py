c54140dd14e40516b512a30a575722bb2b0ebf6c
LindaMunarriz <crea.lindam@gmail.com>
2025-06-17 15:39:00 -0500

    Create streamlit_app.py
    Primera versión de la app de Pulseritas Co.✨

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Pulseritas Co", page_icon="🧿")
+
+st.title("Bienvenida a Pulseritas Co. 🧵✨")
+st.markdown("""
+¡Hola linda! 💗 Esta es nuestra app para reportar ventas, ver métricas y seguir alimentando sonrisas.
+
+Selecciona una opción del menú lateral 👈
+""")
