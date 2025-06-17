823b332da2fa284cae6b34f3d64db858e51039ab
LindaMunarriz <crea.lindam@gmail.com>
2025-06-17 16:50:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 from utils.login import login
 
-# Inicializar variables de sesión
-if 'logueado' not in st.session_state:
+# Inicializamos variables de sesión
+if "logueado" not in st.session_state:
     st.session_state.logueado = False
-if 'usuario' not in st.session_state:
-    st.session_state.usuario = ''
 
-# Mostrar login o bienvenida
+# Login
 if not st.session_state.logueado:
     login()
 else:
-    st.success(f"¡Hola, {st.session_state.usuario.capitalize()}! 🌈✨")
-    st.write("Esta es nuestra app para reportar ventas, ver métricas y seguir alimentando sonrisas.")
-    st.write("Usa el menú lateral para navegar. 🧵🪡")
+    st.sidebar.success(f"✨ ¡Bienvenida, {st.session_state.usuario}!")
