61f8fabafe9111bf77a33293bd7169f738c21fea
LindaMunarriz <crea.lindam@gmail.com>
2025-06-17 20:29:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,17 @@
+# streamlit_app.py
 import streamlit as st
-from utils.login import login
+from utils.login import login, check_login
 
 st.set_page_config(page_title="Pulseritas Co", layout="centered")
 
-# Llamamos a la función login. Esta función:
-# - Muestra el formulario si no estás logueada
-# - Guarda el estado y rerun si sí estás logueada
-login()
+# Mostramos login si no ha iniciado sesión
+if not check_login():
+    login()
+    st.stop()  # IMPORTANTE: evita que el resto del código se ejecute sin login
 
-# Si el login fue exitoso, esta parte ya se ejecuta en el "segundo intento"
+# Si sí está logueada, ahora sí mostramos el menú
 st.sidebar.title("📁 Navegación")
 st.sidebar.success("Selecciona una página a la izquierda ☝️")
+
+st.write(f"¡Hola, {st.session_state.usuario.capitalize()}! 🌈✨")
+st.write("Esta es nuestra app para reportar ventas, ver métricas y seguir alimentando sonrisas.")
