959ef19192c4f27b37b355d34f8a8c2109399e4a
LindaMunarriz <crea.lindam@gmail.com>
2025-06-17 19:56:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,12 @@
-# streamlit_app.py
 import streamlit as st
-from utils import login, auth
+from utils.login import login
 
-# Configuración de la página (título, ícono)
-st.set_page_config(
-    page_title="Pulseritas Co",
-    page_icon="💖"
-)
-
-# Inicializa el estado de la sesión si no existe
+# Inicializamos variables de sesión
 if "logueado" not in st.session_state:
     st.session_state.logueado = False
 
-# Muestra el formulario de login si el usuario no está autenticado
+# Login
 if not st.session_state.logueado:
-    login.mostrar_login()
+    login()
 else:
-    # Si el usuario ya está logueado, muestra la bienvenida y el botón de logout
     st.sidebar.success(f"✨ ¡Bienvenida, {st.session_state.usuario}!")
-    
-    st.title("💖 Bienvenida a Pulseritas Co 💖")
-    st.write("Usa el menú de la izquierda para navegar entre las páginas.")
-    st.info("👈 Selecciona **'Registrar Venta'** para empezar.")
-    
-    auth.mostrar_logout()
-
